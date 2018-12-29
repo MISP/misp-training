2192c8ce471c5eefb03427febbde2982281359eb
Alexandre Dulaunoy <a@foo.be>
2018-12-29 16:27:01 +0100

    chg: [build] make script more generic

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+#
+
+slidedecks=("0-misp-introduction-to-information-sharing")
+mkdir output
 export TEXINPUTS=::`pwd`/themes/
 echo ${TEXINPUTS}
-cd 0-misp-introduction-to-information-sharing
-pdflatex infosharing-introduction.tex
+for slide in ${slidedecks[@]}; do
+        cd ${slide}
+        pdflatex slide.tex
+        pdflatex slide.tex
+        rm *.aux *.toc *.snm *.log *.out *.nav
+        cp slide.pdf ../output/${slide}.pdf
+done
