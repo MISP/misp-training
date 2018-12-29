2209f4380fa87cc80da30052dc48c392acb81284
Alexandre Dulaunoy <a@foo.be>
2018-12-29 16:39:05 +0100

    chg: [build] add ack page and update PDF metadata

--- build.sh
+++ build.sh
@@ -11,4 +11,19 @@ for slide in ${slidedecks[@]}; do
         pdflatex slide.tex
         rm *.aux *.toc *.snm *.log *.out *.nav
         cp slide.pdf ../output/${slide}.pdf
+        cd ..
 done
+
+echo "Generating ack page..."
+cd complementary/ack
+pdflatex ack.tex
+rm *.aux *.log *.out
+cp ack.pdf ../../output
+cd ../..
+
+echo "Generating handout..."
+cd output
+pdfunite *.pdf ../misp-training.pdf
+cd ..
+exiftool -Title="MISP Training and Slide Decks" -Author="CIRCL Computer Incident Response Center Luxembourg" -Subject="MISP Threat Intelligence Platform Training Materials" -Keywords="MISP Threat Intelligence CTI STIX information sharing yara sigma suricata snort bro openioc threat-actor TIP threat intelligence platform circl.lu training cybersecurity MISPProject" misp-training.pdf
+
